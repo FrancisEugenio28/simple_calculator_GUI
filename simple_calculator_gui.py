16e93cfc605f648ff112ee6aa5502e9c29501776
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-05-09 22:26:33 +0800

    Pseudocode

---
+++ simple_calculator_gui.py
@@ -0,0 +1,26 @@
+#Francis Niño A. Eugenio
+#BSCPE 1-5 | Object Oriented Programing
+#Simple Calculator w/ GUI
+
+#create a GUI for our calculator
+    #create a space where the calculation takes place or the screen of a calculator
+    # create a functioning button for 7
+    # create a functioning button for 8 
+    # create a functioning button for 9
+    # create a functioning button for +
+    # create a functioning button for 4
+    # create a functioning button for 5
+    # create a functioning button for 6
+    # create a functioning button for -
+    # create a functioning button for 1
+    # create a functioning button for 2
+    # create a functioning button for 3
+    # create a functioning button for *
+    # create a functioning button for clear
+    # create a functioning button for 0
+    # create a functioning button for =
+    # create a functioning button for /
+# give function for each number button
+# gather all the input digits based on the pressed button in the GUI
+# Give a function for the clear button
+# Perform operation within the = button
